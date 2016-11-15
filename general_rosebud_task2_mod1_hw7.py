0beb15dee89f4788edfbdee48dadf68cca216501
Seth Johns <sethjohns@mail.weber.edu>
2016-11-15 15:33:44 -0700

    finished the bar code stuff

--- general_rosebud_task2_mod1_hw7.py
+++ general_rosebud_task2_mod1_hw7.py
@@ -1,8 +1,53 @@
 #!/usr/bin/env python3
+'''
+Takes a zip code and changes it into a barcode
+'''
 import sys
+
+
+def printDigit(d):
+    '''
+    Tests the print bar function with preset zipCodes
+        Args:
+            d is a zipcode
+    '''
+    print('Enter a zipcode: ',d)
+    printBarCode(d)
+
+
+def printBarCode(zipCode):
+    '''
+    Finds the offset of a zip code and prints a barcode
+    Args:
+        zipCode is a five digit string        
+    '''
+
+    sm = 0
+    barCode = '|'
+    convertBar = ['||:::',':::||','::|:|','::||:',':|::|',':|:|:',':||::','|:::|','|::|:','|:|::']
+    for i in range(0,5):
+        sm += int(zipCode[i])
+        barCode += convertBar[int(zipCode[i])]
+
+    sm = 10 - sm % 10 
+    if sm == 10:
+        sm = 0
+    zipCode += str(sm)
+    barCode += convertBar[sm]+'|'
+    print(barCode)
+    
+
+
+
+def getInput():
+    zipCode = input('Enter a zipcode: ')
+    printBarCode(zipCode)
+
+
 
 # Main Function
 def main():
+    getInput()
     return
 
 if __name__ == '__main__':
--- general_rosebud_task2_mod2_hw7.py
+++ general_rosebud_task2_mod2_hw7.py
@@ -1,8 +1,18 @@
 #!/usr/bin/env python3
 import sys
+import general_rosebud_task2_mod1_hw7 as rose
+
+
+def testingZip():
+    text = open('./barCodeData.txt')
+    zips = text.read()
+    for i in range(0,21,6):
+        rose.printDigit(zips[i:i+5])
+
 
 # Main Function
 def main():
+    testingZip()
     return
 
 if __name__ == '__main__':
